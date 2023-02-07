89b7b40803731a51d4550fcc93f76b7a2b0d6643
unknown <alan.sourek@gmail.com>
2023-02-07 18:22:49 -0500

    Extra Credit

---
+++ main.js
@@ -0,0 +1,9 @@
+let clicks = 0
+
+function buttonClicked() {
+	clicks += 1
+	console.log(clicks)
+	let newText = "Number of times button clicked: " + clicks;
+	let buttonDiv = document.getElementById("button-div");
+	buttonDiv.innerHTML = newText;
+}
